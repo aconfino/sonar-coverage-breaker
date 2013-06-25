8c1fc54e82bc76486f74d31628313d19b9aa3e55
unknown <aconfino@gmail.com>
2013-06-24 20:17:47 -0400

    init

--- src/main/java/com/confino/plugins/sonar/CoverageBreaker.java
+++ src/main/java/com/confino/plugins/sonar/CoverageBreaker.java
@@ -1,6 +1,10 @@
 package com.confino.plugins.sonar;
 
-import java.util.Random;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +16,7 @@ import org.sonar.api.resources.Project;
 public class CoverageBreaker extends BuildBreaker {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CoverageBreaker.class);
-
+	private static final String COVERAGE_PROPERTY_KEY = "coverage-threshhold";
 	private final Settings settings;
 
 	public CoverageBreaker(Settings settings) {
@@ -23,15 +27,47 @@ public class CoverageBreaker extends BuildBreaker {
 		if (settings.getBoolean(CoverageBreakerPlugin.SKIP_KEY)) {
 			LOG.debug("CoverageBreaker disabled on project " + project);
 		} else {
-			compareCoverage();
+			analyzeCoverage();
 		}
 	}
 
-	private void compareCoverage() {
-		int value = new Random().nextInt(100);
-		if (value < 50) {
-			fail("Coverage is less than " + value + ".");
+	private void analyzeCoverage() {
+		Integer coverageThreshhold = parseThreshhold();
+		Integer coverageActual = getCoverageActual();
+		if (coverageActual != null && coverageThreshhold != null && coverageActual < coverageThreshhold) {
+			fail("Coverage is less than the threshhold of " + coverageThreshhold + ".");
 		}
+	}
+	
+	private Integer parseThreshhold(){
+		File propertyFile = new File("build/build-project.properties");
+		if (propertyFile.exists()){
+			return getCoverageThreshhold(propertyFile);
+		}
+		return null;
+	}
+	
+	private Integer getCoverageThreshhold(File file){
+		Properties prop = new Properties();
+		Integer threshhold = null;
+		try {
+			prop.load(new FileInputStream(file));
+			if (prop.containsKey(COVERAGE_PROPERTY_KEY)){
+				threshhold = Integer.valueOf(prop.getProperty(COVERAGE_PROPERTY_KEY));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return threshhold;
+	}
+	
+	private Integer getCoverageActual(){
+		// hardcoded for now...
+		return new Integer(70);
 	}
 
 }
